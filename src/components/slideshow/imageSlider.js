43b0353b652cca1143d77408ee715f7e989f65ce
Master111a <116822664+Master111a@users.noreply.github.com>
2023-04-09 21:47:23 +0700

    Add files via upload

---
+++ src/components/slideshow/imageSlider.js
@@ -0,0 +1,26 @@
+import First from "./images/First.png";
+import Second from "./images/Second.png";
+import Third from "./images/Third.png";
+import Four from "./images/Four.png";
+export default [
+  {
+    title: "First Slider Image",
+    description: "This is the first Slider Image",
+    urls: First,
+  },
+  {
+    title: "Second Slider Image",
+    description: "This is the Second Slider Image",
+    urls: Second,
+  },
+  {
+    title: "Third Slider Image",
+    description: "This is the Third Slider Image",
+    urls: Third,
+  },
+  {
+    title: "Four Slider Image",
+    description: "This is the Four Slider Image",
+    urls: Four,
+  },
+];
